1a442ece86bbf9a2b8d9b09e4518c814e34e3509
Kwame Yamgnane <kw@qwasar.io>
2021-12-28 10:30:02 +0100

    adding licence, contribution, changelog and doc, package configuration in mix

---
+++ CONTRIBUTING.md
@@ -0,0 +1,15 @@
+# Contributing
+
+## Pull Requests Welcome :D :D 
+
+1. Fork ueberauth_quickbooks
+2. Create a topic branch
+3. Make logically-grouped commits with clear commit messages
+4. Push commits to your fork
+5. Open a pull request against ueberauth_quickbooks/main
+
+## Issues
+
+If you believe there to be a bug, please provide the maintainers with enough
+detail to reproduce or a link to an app exhibiting unexpected behavior. For
+help, please start with Stack Overflow.
--- mix.exs
+++ mix.exs
@@ -1,28 +1,55 @@
 defmodule UeberauthQuickbooks.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/Kwame42/ueberauth_quickbooks"
+  
   def project do
     [
       app: :ueberauth_quickbooks,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      docs: docs(),
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :oauth2, :ueberauth]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:oauth2, "~> 1.0 or ~> 2.0"},
+      {:ueberauth, "~> 0.7.0"}
+    ]
+  end
+  
+  defp package do
+    [
+      description: "An Uberauth strategy for Intuit Quickbooks authentication.",
+      files: ["lib", "mix.exs", "README.md", "CHANGELOG.md", "CONTRIBUTING.md", "LICENSE"],
+      maintainers: ["Kwame Yamgnane"],
+      licenses: ["MIT"],
+      links: %{
+        Changelog: "https://hexdocs.pm/ueberauth_quickbooks/changelog.html",
+        GitHub: @source_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["CHANGELOG.md", "CONTRIBUTING.md", "README.md"],
+      main: "readme",
+      source_url: @source_url,
+      homepage_url: @source_url,
+      formatters: ["html"]
     ]
   end
 end
